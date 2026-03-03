024a3e27baa8f5b1f64fa59918735c2c6455874b
hackeroveshhh-hash <hackeroveshhh@gmail.com>
2026-03-03 12:08:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,53 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-import asyncio
-import random
+import asyncio, logging
 from config import Config
-from pyrogram import Client, idle, filters
+from pyrogram import Client as VJ, idle
+from typing import Union, Optional, AsyncGenerator
 from logging.handlers import RotatingFileHandler
 from plugins.regix import restart_forwards
 
-# Create Bot Client
-VJBot = Client(
-    "VJ-Forward-Bot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    sleep_threshold=120,
-    plugins=dict(root="plugins")
-)
-
-# 🔥 Positive Emojis List
-positive_emojis = [
-    "🔥", "🚀", "😎", "💪", "✨",
-    "🎉", "🥳", "⚡", "😃", "🫶"
-]
-
-# ✅ /start Command Handler
-@VJBot.on_message(filters.command("start") & filters.private)
-async def start_handler(client, message):
-    random_emoji = random.choice(positive_emojis)
-    
-    await message.reply_text(
-        f"🚀 Welcome To Advance Forward Bot {random_emoji}\n\n"
-        "Send me any message and I will forward it automatically!"
-    )
-
-async def main():
-    await VJBot.start()
-    await restart_forwards(VJBot)
-    print("Bot Started Successfully ✅")
-    await idle()
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 if __name__ == "__main__":
+    VJBot = VJ(
+        "VJ-Forward-Bot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        sleep_threshold=120,
+        plugins=dict(root="plugins")
+    )  
+
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially."""
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
+               
+    async def main():
+        await VJBot.start()
+        bot_info  = await VJBot.get_me()
+        await restart_forwards(VJBot)
+        print("Bot Started.")
+        await idle()
+
     asyncio.get_event_loop().run_until_complete(main())
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
